b8bf05d3606a7f5fd6db1a68b23ca888192d5a66
Manav Misra <manavm@visionwebsoft.com>
2021-03-21 09:45:51 -0500

    :lipstick: Home page
    :lipstick: Grayscale hero
    Padding, etc.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import { ColorModeSwitcher } from 'ColorModeSwitcher';
 
 const Header = () => (
   <header className="flex justify-between">
-    <Heading fontSize="2xl" className="text-green-500">
+    <Heading fontSize="2xl" className="pl-2 text-green-500">
       InvestTrack
     </Heading>
     <ColorModeSwitcher />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,13 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 
-const Home = () => <Text fontSize="md">Hello Capstone Home Page!</Text>;
+const Home = () => (
+  <Box w="100vw">
+    <Image
+      src="https://images.unsplash.com/photo-1615948074366-969c6745f492?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+      alt="🛤️"
+      className="grayscale"
+    />
+  </Box>
+);
 
 export default Home;
