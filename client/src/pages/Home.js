a472ccc15b8f77873423f8233f0f09b24dd788ae
Manav Misra <manavm@visionwebsoft.com>
2021-03-21 09:45:51 -0500

    :lipstick: Home page
    :lipstick: Grayscale hero
    Padding, etc.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,9 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Text } from '@chakra-ui/react';
 
-const Home = () => <Text fontSize="md">Hello Capstone Home Page!</Text>;
+const Home = () => (
+  <Box className="bg-center bg-cover bg-hero-image h-96 w-screen">
+    <Text>Hello</Text>
+  </Box>
+);
 
 export default Home;
