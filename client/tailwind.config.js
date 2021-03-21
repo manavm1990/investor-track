ca9adad001efa0350649e2b04eb33e1b82c7d56a
Manav Misra <manavm@visionwebsoft.com>
2021-03-21 16:18:21 -0500

    :lipstick: Use IX param to grayscale image

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'hero-image':
-          "url('https://images.unsplash.com/photo-1615948074366-969c6745f492?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80?sat=0')",
+          "url('https://images.unsplash.com/photo-1615948074366-969c6745f492?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80&sat=-80')",
       }),
     },
   },
