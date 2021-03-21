2069ee6680689ac7428991f87dcee41e5ebc8de3
Manav Misra <manavm@visionwebsoft.com>
2021-03-20 20:03:19 -0500

    :fire: Remove comment

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -14,13 +14,7 @@ module.exports = {
     "import/no-unresolved": [
       1,
       {
-        ignore: [
-          // TODO: Add/remove 🔥 additional directories that you may want to use for 'absolute imports'
-          "api",
-          "config",
-          "db",
-          "lib",
-        ],
+        ignore: ["api", "config", "db", "lib"],
       },
     ],
     "import/prefer-default-export": 1,
