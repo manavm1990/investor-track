91e430b71395e9a1b20ab59ebf189ebcf183c823
Manav Misra <manavm@visionwebsoft.com>
2021-03-22 18:11:34 -0500

    :bug: Actually get the records for the user

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,15 +2,30 @@ import config from "config";
 import client from "./client";
 
 export default {
+  /**
+   * Use the ✉️ to find the relevent investment data.
+   * Note 🎵: If this matches the 'env admin,' we get all investments
+   * @param {string} user - ✉️
+   * @returns {[Object]}
+   */
   findInvestments(user) {
+    // TODO: 🐛 Fix this code to avoid constant opening and closing of the connection
     return client.connect().then(async () => {
       try {
         const cursor = await client
           .db("investments")
           .collection("data")
 
-          // If it's the admin, 'find all'
-          .find(user === config.admin ? null : user);
+          /**
+           * If the user is the 'admin,'
+           * we pass `null` into the `find`
+           * thereby retrieving all of the data.
+           *
+           * Otherwise, we are 'finding in array'
+           * only where the investor ✉️ matches our current user.
+           * (https://docs.mongodb.com/manual/tutorial/query-arrays/#query-an-array-for-an-element)
+           */
+          .find(user === config.admin ? null : { "investors.email": user });
         const results = await cursor.toArray();
         return results;
       } catch (error) {
