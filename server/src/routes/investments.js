b3daa1e1c22ef792f7a41345e53c2290855570f2
Manav Misra <manavm@visionwebsoft.com>
2021-03-22 15:06:20 -0500

    `investments` router GET test endpoint

--- server/src/routes/investments.js
+++ server/src/routes/investments.js
@@ -3,6 +3,10 @@ import { Router } from "express";
 
 const router = new Router();
 
+router.get("/", (_, res) => {
+  res.send("<h1>Hello from Investments GET Router</h1>");
+});
+
 // Get all investments for all investors
 router.get("/admin", async (_, res) => {
   // TODO: Wrap this in `try-catch` for error handling.
