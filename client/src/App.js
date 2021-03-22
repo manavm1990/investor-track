54b321ae5e2418bc55b6233303265725f56face7
Manav Misra <manavm@visionwebsoft.com>
2021-03-21 20:17:21 -0500

    Use `useContext` to manage login/logout
    Less `useEffect`

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { Box, ChakraProvider, extendTheme, Grid } from '@chakra-ui/react';
+import api from 'api';
+import { AuthContext } from 'context';
 import Layout from 'layout';
 import { DashboardPage, HomePage } from 'pages';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Fonts } from 'theme';
 import './index.css';
@@ -13,23 +16,40 @@ const theme = extendTheme({
 });
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const user = await api.auth.show();
+      if (user) {
+        setLoggedInUser(user);
+      }
+    })();
+  });
+
+  function toggleUser(user) {
+    setLoggedInUser(() => user || null);
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box>
         <Grid minH="100vh" p={3}>
-          <Layout>
-            <Fonts />
-            <Router>
-              <Switch>
-                <Route exact path="/">
-                  <HomePage />
-                </Route>
-                <Route exact path="/dashboard">
-                  <DashboardPage />
-                </Route>
-              </Switch>
-            </Router>
-          </Layout>
+          <AuthContext.Provider value={{ loggedInUser, toggleUser }}>
+            <Layout>
+              <Fonts />
+              <Router>
+                <Switch>
+                  <Route exact path="/">
+                    <HomePage />
+                  </Route>
+                  <Route path="/dashboard">
+                    <DashboardPage />
+                  </Route>
+                </Switch>
+              </Router>
+            </Layout>
+          </AuthContext.Provider>
         </Grid>
       </Box>
     </ChakraProvider>
