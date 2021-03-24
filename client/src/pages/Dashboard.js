23b935120552280d0597e46a916febbb8bb348e4
Manav Misra <manavm@visionwebsoft.com>
2021-03-23 21:58:03 -0500

    :construction: Load and show error

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,15 +1,19 @@
+import { Box, Skeleton } from '@chakra-ui/react';
 import api from 'api';
 import { AuthContext } from 'context';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
+import { useQuery } from 'react-query';
 import { useHistory } from 'react-router-dom';
 
 function Dashboard() {
   const { loggedInUser } = useContext(AuthContext);
 
-  // TODO: Replace this with `react-query`
-  const [data, setData] = useState([]);
-
   const history = useHistory();
+
+  const fetchInvestments = async () => {
+    const results = await api.db.index({ email: loggedInUser?.email });
+    return results;
+  };
 
   useEffect(() => {
     if (!loggedInUser) {
@@ -17,21 +21,33 @@ function Dashboard() {
     }
   }, [history, loggedInUser]);
 
-  useEffect(() => {
-    (async () => {
-      const user = loggedInUser;
-      const resp = await api.db.index({ email: user?.email });
-      setData(() => resp);
-    })();
-  }, [loggedInUser]);
+  const { isLoading, isError, data, error } = useQuery(
+    'investments',
+    fetchInvestments
+  );
+
+  if (isError) {
+    return <Box className="text-red-500">{error.message}</Box>;
+  }
 
   return (
-    <p>
-      Hello,&nbsp;
-      {loggedInUser?.email === process.env.REACT_APP_INVESTMENTS_ADMIN
-        ? 'Admin'
-        : loggedInUser?.name}
-    </p>
+    <>
+      <p>
+        Hello,&nbsp;
+        {loggedInUser?.email === process.env.REACT_APP_INVESTMENTS_ADMIN
+          ? 'Admin'
+          : loggedInUser?.name}
+      </p>
+
+      <Skeleton
+        isLoaded={!isLoading}
+        startColor="orange.100"
+        endColor="orange.500"
+        className="my-4 h-8 w-4/5"
+      >
+        <p>hello</p>
+      </Skeleton>
+    </>
   );
 }
 
